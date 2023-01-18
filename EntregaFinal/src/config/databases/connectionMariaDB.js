01dfe59de141364fbcdcdcd69f763e4c064b0a77
Diego Martin Villalba <98064949+DiegoMVillalba@users.noreply.github.com>
2023-01-18 11:27:23 -0300

    Delete EntregaFinal directory

--- EntregaFinal/src/config/databases/connectionMariaDB.js
+++
@@ -1,9 +0,0 @@
-import { MARIADB_CONNECTION } from "../index.js";
-
-const connectionMariaDB = {
-  client: 'mysql',
-  connection: MARIADB_CONNECTION,
-  pool: { min: 0, max: 7 },
-};
-
-export default connectionMariaDB;
